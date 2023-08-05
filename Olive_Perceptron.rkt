b217c5e2e694827c2ab5d11f5e0abb20caa6570a
teeoladipo <132300657+teeoladipo@users.noreply.github.com>
2023-08-05 15:54:32 -0400

    Update Olive_Perceptron.rkt

--- Olive_Perceptron.rkt
+++ Olive_Perceptron.rkt
@@ -15,7 +15,7 @@
                          [0.3 0.6 -1.0]
                          [2.0 3.0 1.0]
                          [4.9 3.2 1.0]
-                         [5.0 0.2 1.0]
+                         [5.0 0.2 1.0]  ;; I don't think this function is necessary since you are going to be testing the two sets seperately. -tee
                          [3.0 1.2 1.0]
                          [3.1 0.2 1.0]
                          [1.0 1.1 -1.0]
